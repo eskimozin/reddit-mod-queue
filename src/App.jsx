4f834d84be0e330fd4c7d0ec5e5826208500d989
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-18 00:33:23 -0300

    init: init repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <main
+      className="flex flex-col items-center justify-center min-h-svh [&_*]:tracking-[0.5px]">
+      <div className="px-4 md:px-40 lg:px-60 w-full">
+        <section className={"xl:px-20 2xl:px-80 my-16"}>
+          <h1 className={"text-2xl font-medium p-0 mb-8 text-center"}>...</h1>
+          
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi iste facere suscipit corporis rem nesciunt a. Itaque, impedit, ipsum officiis distinctio tenetur ducimus, maxime natus odio modi consequuntur ratione sequi? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis adipisci vitae asperiores nam ipsa, necessitatibus eius repellat qui quod repellendus pariatur provident? Quibusdam inventore itaque consequuntur veniam mollitia ut dolorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ex sunt obcaecati velit natus nulla commodi consequatur, ab fugiat enim molestias in, neque expedita omnis iste. Alias magni eum officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam distinctio eos sapiente ex hic delectus eum alias voluptatum, a ut quaerat quos repellendus tempora rem! Sunt quas accusamus fuga hic. Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quo possimus beatae voluptatem similique consectetur corrupti quis, a voluptates nulla rem distinctio ipsa laudantium ducimus optio aliquam adipisci repellendus ab!
+          </p>
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
