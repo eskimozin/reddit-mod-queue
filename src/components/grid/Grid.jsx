510c32aae8f38cfec479ef2cbf06eab3742b3f9e
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-20 22:49:12 -0300

    feat: estilizacao dos grid-items

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default function Grid({children}) {
   return (
-    <div className={"group mx-auto grid max-w-sm items-start gap-6 lg:max-w-none lg:grid-cols-4"}>
+    <div className={"group grid max-w-sm items-start gap-6 md:mx-auto lg:mx-auto lg:max-w-none lg:grid-cols-4"}>
       {children}
     </div>
   )
