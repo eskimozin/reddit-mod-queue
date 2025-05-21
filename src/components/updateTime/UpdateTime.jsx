3d3c053b927e819fc6299c4fa7211b91e61fda10
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-21 20:35:49 -0300

    feat: logica corrigida

--- src/components/updateTime/UpdateTime.jsx
+++ src/components/updateTime/UpdateTime.jsx
@@ -12,14 +12,18 @@ export default function UpdateTime({time}) {
     const momentNow = moment.tz('America/Sao_Paulo');
     
     const seconds = momentNow.diff(momentTime, "seconds");
-    const minutes = momentNow.diff(momentTime, "minutes");
+    let minutes = momentNow.diff(momentTime, "minutes");
     let hours = momentNow.diff(momentTime, "hours");
     
     // Implementação para forçar a apresentação de horário correto em produção
     hours = window.location.hostname === "localhost" ? hours : hours - 3;
+    
+    // Se os minutos forem maior ou igual a 180 será subtraído 180 minutos (60 * 3), pois no caso de UTC ser 0, o horário estaria 3 horas a frente
+    minutes = window.location.hostname !== "localhost" && minutes >= 180 ? minutes - (60 * 3) : minutes;
+    
     // Se o horário for menor que 4 horas, os minutos serão exibidos e a hora não. Para corrigir isso, no ambiente de produção, é feita a divisão dos minutos por 60, resultando na quantidade de horas
     // As horas então, são apresentadas, ao invés dos minutos
-    hours = window.location.hostname !== "localhost" && minutes > 60 && hours < 4 ? Math.floor(minutes / 60) : hours
+    hours = window.location.hostname !== "localhost" && minutes >= 60 && hours < 4 ? Math.floor(minutes / 60) : hours
     
     console.log(seconds, minutes, hours, momentTime.diff(momentTime, "hours"), momentTime.diff(momentTime, "minutes"));
     
