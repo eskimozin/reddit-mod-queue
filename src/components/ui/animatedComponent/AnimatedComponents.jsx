00784a929cf26e76e1cbd8bfd523712d47dc92d6
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-22 06:37:44 -0300

    refactor: prop "as" removida

--- src/components/ui/animatedComponent/AnimatedComponents.jsx
+++ src/components/ui/animatedComponent/AnimatedComponents.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 import AnimatedComponent from "./AnimatedComponent.jsx";
 import {AnimatePresence} from "framer-motion";
 
-const AnimatedComponents = ({children, as}) => {
+const AnimatedComponents = ({children}) => {
   let ret;
-
+  
   ret = Array.isArray(children) ? children.map((child, key) => {
     return (
       <AnimatePresence mode={"wait"} key={key}>
