697871f7b3939b05d6c6f452b3e5adf37c5a5851
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-07-08 22:15:40 -0300

    feat: hosts ajustados

--- src/config.js
+++ src/config.js
@@ -7,8 +7,10 @@ export default {
     },
     "developer-name": "Gabriel",
   },
-  "host": window.location.hostname === "localhost" ? "http://localhost:8001" : localStorage.getItem("prod-test") ? "https://resplendent-enjoyment-test.up.railway.app" : "https://gabriers.up.railway.app",
-  "modQueueServer": window.location.hostname === "localhost" ? "http://localhost:8010" : localStorage.getItem("prod-test") ? "https://server-reddit-mod-queue-test.up.railway.app" : "https://server-reddit-mod-queue-production.up.railway.app",
+  // "host": window.location.hostname === "localhost" ? "http://localhost:8001" : localStorage.getItem("prod-test") ? "https://resplendent-enjoyment-test.up.railway.app" : "https://gabriers.up.railway.app",
+  "host": window.location.hostname === "localhost" ? "http://localhost:8001" : "https://gabriers.up.railway.app",
+  // "modQueueServer": window.location.hostname === "localhost" ? "http://localhost:8010" : localStorage.getItem("prod-test") ? "https://server-reddit-mod-queue-test.up.railway.app" : "https://server-reddit-mod-queue-production.up.railway.app",
+  "modQueueServer": window.location.hostname === "localhost" ? "http://localhost:8010" : "https://server-reddit-mod-queue-production.up.railway.app",
   "links": {
     "report": "https://github.com/eskimozin/reddit-mod-queue/issues/new",
     "subreddit": "https://reddit.com/r/eskimozin",
