75c576140ff88c8a7cdcb1d85274413fc1c99e97
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-08-02 15:24:57 -0300

    feat: melhoria da apresentacao das mensagens, usando cor verde para exibicao das mensagens

--- src/components/ui/validator/Validator.jsx
+++ src/components/ui/validator/Validator.jsx
@@ -12,11 +12,11 @@ export default function Validator() {
   const [token, setToken] = useState("");
   const [message, setMessage] = useState("");
   const captchaRef = useRef(null);
-  
+
   useEffect(() => {
     captchaRef?.current?.resetCaptcha();
   }, [])
-  
+
   const confirmAction = async ({token, id, action, code, datetimeCreate}) => {
     const res = await fetch(`${config.modQueueServer}/api/verify-code`, {
       method: 'POST',
@@ -29,10 +29,10 @@ export default function Validator() {
         datetimeCreate
       }),
     });
-    
+
     const data = await res.json();
     // console.log(data);
-    
+
     if (data.success) {
       console.log("OK!");
       setMessage("Tudo certo! Em instantes a solicitação será concluída. É só aguardar. Você pode fechar a página. Quando a solicitação tiver sido concluída, uma mensagem será enviada no canal de logs do Discord.");
@@ -42,32 +42,32 @@ export default function Validator() {
       captchaRef.current.resetCaptcha();
     }
   }
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    
+
     if (!token) {
       setMessage('Por favor, complete o captcha antes de enviar.');
       return;
     }
-    
+
     let [id, datetimeCreate, action, code] = params.id.split('A');
     datetimeCreate = moment(datetimeCreate, "x").format("YYYY-MM-DD HH:mm:ss");
     console.log({id, datetimeCreate, action, code})
-    
+
     confirmAction({token, id, action, code, datetimeCreate}).then(() => {
       console.log("OK!");
     });
   }
-  
+
   return (
     <div className="flex align-center justify-center">
       <div className="container mx-3 my-8 lg:my-10 min-h-[90vh] flex justify-between flex-col">
         <div className={"max-w-[580px] mx-auto"}>
           <main className="flex justify-center flex-col gap-12 items-center">
             <h1 className={"text-balance text-center font-bold text-3xl"}>Preencha o captcha para finalizar a validação</h1>
-            
+
             <form className="flex justify-center flex-col" onSubmit={handleSubmit}>
               <div className={"mx-auto"}>
                 <HCaptcha
@@ -78,17 +78,17 @@ export default function Validator() {
                   ref={captchaRef}
                 />
               </div>
-              
+
               {
                 message && (
                   <AnimatedComponents>
-                    <div className="flex flex-wrap gap-2 items-center text-center p-3 bg-orange-500/5 border border-orange-300/25 text-orange-300 mt-4 rounded">
+                    <div className={"flex flex-wrap gap-2 items-center text-center p-3 mt-4 rounded " + (message?.toLowerCase().includes("tudo certo!") ? "bg-green-500/5 border border-green-300/25 text-green-600" : "bg-orange-500/5 border border-orange-300/25 text-orange-300")}>
                       <p className={"text-balance"}>{message}</p>
                     </div>
                   </AnimatedComponents>
                 )
               }
-              
+
               <div className={"mt-4 flex flex-wrap gap-2 items-center justify-center"}>
                 <Button type="submit" className="inline-flex items-center gap-2 rounded-md bg-orange-600 px-3 py-1.5 text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700 focus-headless">
                   Confirmar
