e643063ea73bd874eec70013b0c10de8eaed513a
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-08-17 15:04:44 -0300

    feat: acao de abrir primeiros 10 posts de "postPending" em novas guias

--- src/components/open-all-posts/OpenAllPosts.jsx
+++ src/components/open-all-posts/OpenAllPosts.jsx
@@ -7,7 +7,7 @@ export default function OpenAllPosts() {
   
   return (
     <Button type="button" onClick={() => {
-      if (confirm("Os posts serão abertos em novas janelas. Provavelmente o navegador vai pedir permissão para que esta página possa abrir outras páginas. Você precisa autorizar para que funcione corretamente.")) {
+      if (confirm("Os posts serão abertos em novas guias. Provavelmente o navegador vai pedir permissão para que esta página possa abrir outras páginas. Você precisa autorizar para que funcione corretamente.")) {
         postsPending.toSpliced(10).forEach((post, index) => {
           setTimeout(() => {
             window.open(post?.["reddit_link"], "_blank", "noopener noreferrer");
@@ -15,7 +15,7 @@ export default function OpenAllPosts() {
         })
       }
     }} className="inline-block items-center gap-2 rounded-md bg-slate-800 px-3 py-1.5 text-white focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700 focus-headless">
-      Abrir os primeiros 10 posts da lista em novas janelas
+      Abrir os primeiros 10 posts da lista em novas guias
     </Button>
   )
 }
